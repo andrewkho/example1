ccec5905e5c928941e580559940905860e15e4c5
gsathish <sathish.gopalakrishnan@gmail.com>
2014-09-04 21:41:54 -0700

    Create the FirstProgram class. Yay!

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+
+public class FirstProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println( "Welcome to EECE 210!" );
+		System.out.println( "Was that not easy?" );
+		
+
+	}
+
+}
