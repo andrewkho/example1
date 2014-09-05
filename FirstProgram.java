bc00a968725dc475a3157aa3d76c6dbb4718cc32
gsathish <sathish.gopalakrishnan@gmail.com>
2014-09-04 22:07:24 -0700

    Changed the comment.

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,9 +1,7 @@
 
 public class FirstProgram {
 
-	/**
-	 * @param args
-	 */
+	// Let us print two strings.
 	public static void main(String[] args) {
 		
 		System.out.println( "Welcome to EECE 210!" );
